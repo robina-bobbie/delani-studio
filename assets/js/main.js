35f42c0b826fd1257c89db7474073005f3b3506a
Robina Bosibori <mainarobina@gmail.com>
2023-04-21 12:54:19 +0300

    Started hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,4 +43,93 @@ $(document).ready(() => {
   });
 
 
+  $("#work4").hover(
+    () => {
+      $("#work4").css("opacity", "0.35");
+      $(".hover").css("opacity", "1");
+    },
+    () => {
+      $("#work4").css("opacity", "1");
+      $(".hover").css("opacity", "0");
+    }
+  );
+
+  $("#work3").hover(
+    () => {
+      $("#work3").css("opacity", "0.35");
+      $(".hover1").css("opacity", "1");
+    },
+    () => {
+      $("#work3").css("opacity", "1");
+      $(".hover1").css("opacity", "0");
+    }
+  );
+
+  $("#work2").hover(
+    () => {
+      $("#work2").css("opacity", "0.35");
+      $(".hover2").css("opacity", "1");
+    },
+    () => {
+      $("#work2").css("opacity", "1");
+      $(".hover2").css("opacity", "0");
+    }
+  );
+
+  $("#work1").hover(
+    () => {
+      $("#work1").css("opacity", "0.35");
+      $(".hover3").css("opacity", "1");
+    },
+    () => {
+      $("#work1").css("opacity", "1");
+      $(".hover3").css("opacity", "0");
+    }
+  );
+
+  $("#work5").hover(
+    () => {
+      $("#work5").css("opacity", "0.35");
+      $(".hover4").css("opacity", "1");
+    },
+    () => {
+      $("#work5").css("opacity", "1");
+      $(".hover4").css("opacity", "0");
+    }
+  );
+
+  $("#work6").hover(
+    () => {
+      $("#work6").css("opacity", "0.35");
+      $(".hover5").css("opacity", "1");
+    },
+    () => {
+      $("#work6").css("opacity", "1");
+      $(".hover5").css("opacity", "0");
+    }
+  );
+
+  $("#work7").hover(
+    () => {
+      $("#work7").css("opacity", "0.35");
+      $(".hover6").css("opacity", "1");
+    },
+    () => {
+      $("#work7").css("opacity", "1");
+      $(".hover6").css("opacity", "0");
+    }
+  );
+
+  $("#work8").hover(
+    () => {
+      $("#work8").css("opacity", "0.35");
+      $(".hover7").css("opacity", "1");
+    },
+    () => {
+      $("#work8").css("opacity", "1");
+      $(".hover7").css("opacity", "0");
+    }
+  );
+
+
 });
