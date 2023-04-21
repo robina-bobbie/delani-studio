9a2dfa40182c51867b696a9524dca4efba224cc2
Robina Bosibori <mainarobina@gmail.com>
2023-04-21 12:35:33 +0300

    Added toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,38 @@ window.addEventListener('load', () => {
     mirror: false
   })
 });
+
+$(document).ready(() => {
+
+  // toggle design
+  $("#designImage").click(() => {
+    $("#designImage").toggle();
+    $("#designDescription").toggle();
+  });
+  $("#designDescription").click(() => {
+    $("#designDescription").toggle();
+    $("#designImage").toggle();
+  });
+
+  // toggle development
+  $("#developmentImage").click(() => {
+    $("#developmentImage").toggle();
+    $("#developmentDescription").toggle();
+  });
+  $("#developmentDescription").click(() => {
+    $("#developmentDescription").toggle();
+    $("#developmentImage").toggle();
+  });
+
+  // toggle product
+  $("#productImage").click(() => {
+    $("#productImage").toggle();
+    $("#productDescription").toggle();
+  });
+  $("#productDescription").click(() => {
+    $("#productDescription").toggle();
+    $("#productImage").toggle();
+  });
+
+
+});
